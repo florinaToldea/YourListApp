3cab49d452ebba40c08c0889705151dbcbd2e30f
Florina Toldea <toldea.florina1712@yahoo.com>
2022-05-07 14:52:44 +0200

    feat: map

--- app/src/main/java/com/example/yourlistapp/ViewModel/MapViewModel.java
+++ app/src/main/java/com/example/yourlistapp/ViewModel/MapViewModel.java
@@ -1,4 +1,18 @@
 package com.example.yourlistapp.ViewModel;
 
-public class MapViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MapViewModel extends ViewModel {
+    private final MutableLiveData<String> text;
+
+    public MapViewModel() {
+        this.text = new MutableLiveData<>();
+        text.setValue("This is map fragment");
+    }
+
+    public LiveData<String> getText(){
+        return text;
+    }
 }
